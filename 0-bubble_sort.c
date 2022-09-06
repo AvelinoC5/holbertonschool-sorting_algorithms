46e646ff5a7f71a9926c1e9d9d34687d89081413
AvelinoC5 <avelino_63@hotmail.com>
2022-09-06 17:39:24 -0500

    update task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort algorithms
+ * @size: size input
+ * @array: pointer input
+ * 
+ * Return: No Return
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, n, swap;
+
+	if (size < 2)
+	    return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (n = 0; n < size - i - 1; n++)
+		{
+			if (array[n] > array[n + 1])
+			{
+				swap = array[n];
+				array[n] = array[n + 1];
+				array[n + 1] = swap;
+				print_array(array, size);
+			}
+		}
+	}
+}
