448524731f13b7111caedfc0066cb6325c08a46e
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-09-08 12:30:39 -0500

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -26,32 +26,32 @@ void quick_sort(int *array, size_t size)
 void quick(int *array, int lo, int hi, size_t size)
 {
 	int pivot, i = lo -1, j;
-    int partition = array[hi], aux = 0;
+   	int partition = array[hi], aux = 0;
 	if (lo < hi)
 	{
-        for (j = lo; j < hi; j++)
-        {
-            if (array[j] < partition)
-            {
-                i++;
-                if (array[i] != array[j])
-                {
-                    aux = array[i];
-                    array[i] = array[j];
-                    array[j] = aux;
-                    print_array(array, size);
-                }
-            }
-        }
-        if (array[i + 1] != array[hi])
-        {
-            aux = array[i + 1];
-            array[i + 1] = array[hi];
-            array[hi] = aux;
-            print_array(array, size);
-        }
-        pivot = i + 1;
-        quick(array, lo, pivot - 1, size);
-        quick(array, pivot + 1, hi, size);
+		for (j = lo; j < hi; j++)
+		{
+			if (array[j] < partition)
+			{
+				i++;
+				if (array[i] != array[j])
+				{
+					aux = array[i];
+					array[i] = array[j];
+					array[j] = aux;
+					print_array(array, size);
+				}
+			}
+		}
+		if (array[i + 1] != array[hi])
+		{
+			aux = array[i + 1];
+			array[i + 1] = array[hi];
+			array[hi] = aux;
+			print_array(array, size);
+		}
+		pivot = i + 1;
+		quick(array, lo, pivot - 1, size);
+		quick(array, pivot + 1, hi, size);
     }
 }
